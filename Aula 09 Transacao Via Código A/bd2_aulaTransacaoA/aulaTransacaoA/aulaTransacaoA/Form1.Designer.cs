9a72a51e601b58a2975767b2402356a6558a1299
Eduardo Henrique Marques Ferreira <eduardo@prosanearinfo.com.br>
2021-02-23 20:45:37 -0300

    Carregando comboBox1 com 100 pessoas.

--- Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Form1.Designer.cs
+++ Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Form1.Designer.cs
@@ -29,6 +29,11 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
@@ -41,14 +46,56 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Location = new System.Drawing.Point(53, 122);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(409, 205);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(62, 33);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(225, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pessoas:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(296, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 22);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Buscar dados";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(514, 404);
+            this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +103,10 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Form1.cs
+++ Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Windows.Forms;
 using aulaTransacaoA.Model;
 using aulaTransacaoA.Controller;
+using MySql.Data.MySqlClient;
 
 namespace aulaTransacaoA
 {
@@ -47,6 +48,43 @@ namespace aulaTransacaoA
                                     MessageBoxIcon.Error);
             }
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DadosConexao dadosConexao = new DadosConexao("localhost","root", "123456", "escola", 3306 );
+            ConexaoBD conexaoBD = new ConexaoBD(dadosConexao);
+            comboBox1.Items.Clear();
+            try
+            {
+                if (conexaoBD.conectar())
+                {
+                    string sql = "SELECT * FROM pessoa ORDER BY nome LIMIT 100";
+                    MySqlCommand comando = new MySqlCommand(sql, conexaoBD.conexao);
+                    MySqlDataReader dr = comando.ExecuteReader();
+                    if (dr.HasRows)
+                    {
+                        while (dr.Read())
+                        {
+                            comboBox1.Items.Add(dr["nome"].ToString().ToUpper());
+                        }
+                    }
+                    dr.Dispose();
+                    dr.Close();
+                    conexaoBD.desconectar();
+                }
+                else
+                {
+                    MessageBox.Show("Não conectou!!!", "Aplicação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao buscar pessoas no banco de dados: " + ex.ToString(),
+                                    "Aplicação",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+            }
         }
     }
 }
