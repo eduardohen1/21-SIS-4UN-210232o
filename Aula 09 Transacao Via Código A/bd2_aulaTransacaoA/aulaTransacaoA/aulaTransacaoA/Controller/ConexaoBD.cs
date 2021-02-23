22c251acdf93c675092b2f3729eed777e1acfc4b
Eduardo Henrique Marques Ferreira <eduardo@prosanearinfo.com.br>
2021-02-23 20:07:04 -0300

    Método conectar

--- Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Controller/ConexaoBD.cs
+++ Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Controller/ConexaoBD.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
 
 namespace aulaTransacaoA.Controller
 {
@@ -19,6 +20,46 @@ namespace aulaTransacaoA.Controller
         }
 
         //Método Conectar no banco de dados
+        public bool conectar()
+        {
+            //testar se o objeto dadosConexao é diferente de null
+            if (dadosConexao != null)
+            {
+                try
+                {
+                    //testar se a variável conexão está ativa
+                    if (conexao.State == System.Data.ConnectionState.Open) 
+                        desconectar();
+
+                    string sql = "Server=" + dadosConexao.host.Trim() + ";" +
+                                 "Database=" + dadosConexao.dataBase.Trim() + ";" +
+                                 "Uid=" + dadosConexao.user.Trim() + ";" +
+                                 "Pwd=" + dadosConexao.password.Trim() + ";" +
+                                 "Connection Timeout=900;" +
+                                 "Port=" + dadosConexao.port.ToString();
+                    conexao = new MySqlConnection(sql);
+                    conexao.Open();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao conectar com o banco de dados: " + ex.ToString(),
+                                    "Aplicação",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nenhum dados de conexão encontrado. Verifique!",
+                                    "Aplicação",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Error);
+                return false;
+            }
+        }
+
         //Método Desconectar do banco de dados
 
 
