a98b344aa80ee331a195456c7e80ac4abf51eede
Eduardo Henrique Marques Ferreira <eduardo@prosanearinfo.com.br>
2021-02-23 19:39:07 -0300

    Programa em C# para teste de transação.

---
+++ Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Controller/ConexaoBD.cs
@@ -0,0 +1,26 @@
+﻿using aulaTransacaoA.Model;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aulaTransacaoA.Controller
+{
+    public class ConexaoBD
+    {
+        public MySqlConnection conexao = new MySqlConnection(); //variável de conexão com o BD
+        DadosConexao dadosConexao = null; //variável de dados da conexão
+        
+        //Método construtor
+        public ConexaoBD(DadosConexao dadosConexao)
+        {
+            this.dadosConexao = dadosConexao;
+        }
+
+        //Método Conectar no banco de dados
+        //Método Desconectar do banco de dados
+
+
+    }
+}
---
+++ Aula 09 Transacao Via Código A/bd2_aulaTransacaoA/aulaTransacaoA/aulaTransacaoA/Model/DadosConexao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace aulaTransacaoA.Model
+{
+    public class DadosConexao
+    {
+        public string host { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+        public string dataBase { get; set; }
+        public int port { get; set; }
+
+        //método construtor:
+        public DadosConexao(string host, string user, string password, string dataBase, int port)
+        {
+            this.host = host;
+            this.user = user;
+            this.password = password;
+            this.dataBase = dataBase;
+            this.port = port;
+        }
+
+    }
+}
