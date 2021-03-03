faa53405d500ba6dca594e412d2a05a8236b4966
Eduardo Henrique Marques Ferreira <eduardo@prosanearinfo.com.br>
2021-03-02 21:54:11 -0300

    Criação de objeto ComboboxItem. Botão de teste de idade da pessoa.

--- Aula 09 Transacao Via Código B/bd2_aulaTransacaoB/aulaTransacaoB/aulaTransacaoB/Form1.Designer.cs
+++ Aula 09 Transacao Via Código B/bd2_aulaTransacaoB/aulaTransacaoB/aulaTransacaoB/Form1.Designer.cs
@@ -30,9 +30,11 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -48,6 +50,8 @@
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
             this.groupBox1.Controls.Add(this.button2);
             this.groupBox1.Controls.Add(this.comboBox1);
             this.groupBox1.Controls.Add(this.label1);
@@ -58,23 +62,6 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 42);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(50, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Pessoas:";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(68, 34);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(283, 21);
-            this.comboBox1.TabIndex = 1;
-            // 
             // button2
             // 
             this.button2.Location = new System.Drawing.Point(357, 26);
@@ -84,6 +71,42 @@
             this.button2.Text = "Buscar dados";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(68, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(283, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pessoas:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(357, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 29);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Busca de idade";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(357, 96);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(135, 29);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Criar aluno";
+            this.button4.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -107,6 +130,8 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- Aula 09 Transacao Via Código B/bd2_aulaTransacaoB/aulaTransacaoB/aulaTransacaoB/Form1.cs
+++ Aula 09 Transacao Via Código B/bd2_aulaTransacaoB/aulaTransacaoB/aulaTransacaoB/Form1.cs
@@ -67,7 +67,13 @@ namespace aulaTransacaoB
                     {
                         while (dr.Read())
                         {
-                            comboBox1.Items.Add(dr["nome"].ToString().ToUpper());
+                            //comboBox1.Items.Add(dr["nome"].ToString().ToUpper() + "                                                                                       " +
+                            //                    int.Parse(dr["id_pessoa"].ToString()).ToString("D6")
+                            //    );
+                            ComboboxItem item = new ComboboxItem();
+                            item.Text = dr["nome"].ToString().ToUpper();
+                            item.Value = dr["id_pessoa"].ToString();
+                            comboBox1.Items.Add(item);
                         }
                     }
                     dr.Dispose();
@@ -88,5 +94,63 @@ namespace aulaTransacaoB
                     MessageBoxIcon.Information);
             }
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex >= 0)
+            {
+                ComboboxItem item = (comboBox1.SelectedItem as ComboboxItem);
+                //MessageBox.Show("Id_pessoa: " + item.Value.ToString(), "Aplicação", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                try
+                {
+                    //conectar no banco
+                    DadosConexao dadosConexao = new DadosConexao("localhost", "root", "123456", "escola", 3306);
+                    ConexaoBD conexaoBD = new ConexaoBD(dadosConexao);
+                    if (conexaoBD.conectar())
+                    {
+                        //fazer a seleção dos dados 
+                        string sql = "SELECT *, func_idade(dt_nascimento) idade FROM pessoa WHERE id_pessoa = " + 
+                            item.Value.ToString();
+                        MySqlCommand comando = new MySqlCommand(sql, conexaoBD.conexao);
+                        MySqlDataReader dr = comando.ExecuteReader();
+                        if (dr.HasRows)
+                        {
+                            //retornar msm para o usuário
+                            while (dr.Read())
+                            {
+                                string resposta = "A pessoa " + item.Text + " tem " + 
+                                    dr["idade"].ToString() + " de idade.";
+                                MessageBox.Show(resposta, "Aplicação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                        }
+                        dr.Dispose();
+                        dr.Close();
+                        conexaoBD.desconectar();
+                    }                    
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Erro ao buscar idade de pessoa no Banco de Dados:\n" + ex.ToString(),
+                    "Aplicação",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nenhum registro selecionado.", "Aplicação", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+        }
     }
+
+    public class ComboboxItem
+    {
+        public string Text { get; set; }
+        public object Value { get; set; }
+        public override string ToString()
+        {
+            return Text;
+        }
+    }
+
 }
